886f422f357677b528ed33d2d5c1216c44437f5b
Jeff Hall <rhythmatician5@gmail.com>
2026-01-26 05:45:10 -0600

    fix: change default epochs to 1 in training script

--- backend/train.py
+++ backend/train.py
@@ -57,7 +57,7 @@ def main():
         help="Directory containing audio files",
     )
     parser.add_argument(
-        "--epochs", type=int, default=100, help="Number of training epochs"
+        "--epochs", type=int, default=1, help="Number of training epochs"
     )
     parser.add_argument("--batch-size", type=int, default=32, help="Batch size")
     parser.add_argument(
